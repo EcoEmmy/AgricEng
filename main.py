fec1c7bf76b57f1bc34ed4de8192c99c619aead2
EcoEmmy <emmyffboss@gmail.com>
2025-11-02 13:36:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from app import app, create_tables, seed_database
+
 if __name__ == '__main__':
     try:
-        # Reset the DB once
-        reset_database()
         create_tables()
         seed_database()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    except Exception as e:
+        print(f"Database initialization error: {e}")
 
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
