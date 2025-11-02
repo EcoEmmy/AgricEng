05b15c1adc3cb930f64cbe636aaf20d456d61f1f
EcoEmmy <emmyffboss@gmail.com>
2025-11-02 13:29:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app import app, create_tables, seed_database
-if __name__ == "__main__":
-    create_tables()
-    seed_database()
+if __name__ == '__main__':
+    try:
+        # Reset the DB once
+        reset_database()
+        create_tables()
+        seed_database()
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
